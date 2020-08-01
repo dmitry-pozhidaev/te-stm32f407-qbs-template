fa502086f4085cfae2915e20e3a059b16f2e8350
dm.pozhidaev <dm.pozhidaev@gmail.com>
2020-08-02 01:13:56 +0300

    removed some unused files

--- src/app/ethernet.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef _ETHERNET_H_
-#define _ETHERNET_H_
-
-
-void ethernet_initialize(void);
-
-
-#endif // _ETHERNET_H_
--- src/libs/port/gcc/system_calls.c
+++
@@ -1,7 +0,0 @@
-#include <generic.h>
-#include <stm32f4xx.h>
-
-uint sys_now(void)
-{
-    return system_time() * 1000;
-}
